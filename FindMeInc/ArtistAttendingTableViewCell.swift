c6927155fbe21a06fdc74ac1a0b9eac24274b311
Alex <mr.abazovik@mail.ru>
2017-02-10 15:35:21 +0300

    1

---
+++ FindMeInc/ArtistAttendingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtistAttendingTableViewCell.swift
+//  FindMeInc
+//
+//  Created by Alexandr on 2/10/17.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ArtistAttendingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
