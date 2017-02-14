f4579305409f0ebf7857b3ded1f6ffa0444d9239
Alex <mr.abazovik@mail.ru>
2017-02-14 10:54:38 +0300

    New version

--- FindMeInc/UploadImageViewController.swift
+++ FindMeInc/UploadImageViewController.swift
@@ -2,11 +2,15 @@ import UIKit
 
 class UploadImageViewController: UIViewController {
 
+    @IBOutlet weak var viewToDrag: UIView!
     @IBOutlet weak var waterMark: UILabel!
+    @IBOutlet var panGestureRecognizerForDragWaterMark: UIPanGestureRecognizer!
 
+    var lastLocation: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        lastLocation = waterMark.center
 
     }
 
@@ -24,6 +28,15 @@ class UploadImageViewController: UIViewController {
             waterMark.isHidden = true
         }else{
             waterMark.isHidden = false
+        }
+    }
+    @IBAction func waterMarkDragged(_ sender: UIPanGestureRecognizer) {
+        
+        //To do: add validation to the bounds of the view
+        
+        waterMark.center = CGPoint(x:(lastLocation.x + sender.translation(in: viewToDrag).x), y:(lastLocation.y + sender.translation(in:viewToDrag).y))
+        if sender.state == .ended{
+            lastLocation = waterMark.center
         }
     }
 }
