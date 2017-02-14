511cc81ddc2b21f9f5cbd18d147bea42752fecfd
Alex <mr.abazovik@mail.ru>
2017-02-14 11:54:19 +0300

    Different storyboards

---
+++ MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  FindMeInc
+//
+//  Created by Alexandr on 2/14/17.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    @IBOutlet weak var mainTabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.items?.first?.image = #imageLiteral(resourceName: "FMI_All_Star_Icon")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
