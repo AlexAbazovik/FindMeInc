2f3c73119277c2a1bb9443cc4069f39feb47b727
AlexVecher <alex7vecher@gmail.com>
2017-02-28 13:22:58 +0300

    Edited AddConsultationViewController

--- FindMeInc/AddConsultationViewController.swift
+++ FindMeInc/AddConsultationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddConsultationViewController: UIViewController {
+class AddConsultationViewController: UIViewController, UITextFieldDelegate {
     
     var currentDate: Date!
 
@@ -23,6 +23,7 @@ class AddConsultationViewController: UIViewController {
     @IBOutlet weak var messageTextView: UITextView!
     
     var datePicker: UIDatePicker!
+    var toolbar: UIToolbar!
     var invisibleTextField: UITextField!
     
     //MARK: - UIViewController lifecycle
@@ -31,19 +32,49 @@ class AddConsultationViewController: UIViewController {
         // Do any additional setup after loading the view.
         setBorder()
         setDate(currentDate)
-        invisibleTextField = UITextField()
-        self.view .addSubview(invisibleTextField)
-        datePicker = UIDatePicker(frame: CGRect(x: 0.0, y: 0.0, width: view.frame.width, height: 200.0))
-        datePicker.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.9450980392, blue: 0.9450980392, alpha: 1)
-        datePicker.datePickerMode = .date
-        datePicker.minimumDate = NSDate().addingTimeInterval(86400) as Date
         
+        invisibleTextField = UITextField()
+        self.view.addSubview(invisibleTextField)
+        
+        //custom keyboard
+        datePicker = getPickerView()
+        toolbar = getKeyboardToolbar()
+        
+        invisibleTextField.inputAccessoryView = datePicker
+        invisibleTextField.inputView = toolbar
+        
+        //keyboard notifications
+        registerForKeyboardNotification()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        unregisterForKeyboardNotification()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Custom keyboard
+    //MARK: - UIDatePicker
+    func getPickerView() -> UIDatePicker {
+        let datePickerView = UIDatePicker(frame: CGRect(x: 0.0, y: 0.0, width: view.frame.width, height: 200.0))
+        datePickerView.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.9450980392, blue: 0.9450980392, alpha: 1)
+        datePickerView.datePickerMode = .date
+        datePickerView.minimumDate = NSDate().addingTimeInterval(86400) as Date
+        return datePickerView
+    }
+    
+    //MARK: - UIToolbar
+    func getKeyboardToolbar() -> UIToolbar {
         //Add toolbar to control the picker view
-        let toolBar = UIToolbar()
-        toolBar.barStyle = UIBarStyle.default
-        toolBar.isTranslucent = true
-        toolBar.tintColor = #colorLiteral(red: 0.8980392157, green: 0.6156862745, blue: 0.3803921569, alpha: 1)
-        toolBar.sizeToFit()
+        let keyboardToolBar = UIToolbar()
+        keyboardToolBar.barStyle = UIBarStyle.default
+        keyboardToolBar.isTranslucent = true
+        keyboardToolBar.tintColor = #colorLiteral(red: 0.8980392157, green: 0.6156862745, blue: 0.3803921569, alpha: 1)
+        keyboardToolBar.sizeToFit()
         
         //Add buttons to the toolbar
         let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: self, action: #selector(donePicker))
@@ -51,17 +82,10 @@ class AddConsultationViewController: UIViewController {
         let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: #selector(donePicker))
         cancelButton.tag = 1
         
-        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
-        toolBar.isUserInteractionEnabled = true
+        keyboardToolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        keyboardToolBar.isUserInteractionEnabled = true
         
-        
-        invisibleTextField.inputAccessoryView = datePicker
-        invisibleTextField.inputView = toolBar
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        return keyboardToolBar
     }
     
     //MARK: - UIToolbar actions
@@ -70,6 +94,31 @@ class AddConsultationViewController: UIViewController {
             setDate(datePicker.date)
         }
         invisibleTextField.resignFirstResponder()
+    }
+    
+    //MARK: Register for keyboard notifications
+    func registerForKeyboardNotification(){
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    //MARK: Unregister for keyboard notifications
+    func unregisterForKeyboardNotification(){
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+        let userInfo:NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardFrame:NSValue = userInfo.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keyboardRectangle.height
+        
+        self.view.frame = CGRect(x: 0.0, y: -keyboardHeight / 5.0, width: self.view.bounds.width, height: self.view.bounds.height)
+    }
+    
+    func keyboardWillHide(){
+        self.view.frame = CGRect(x: 0.0, y: 0.0, width: self.view.bounds.width, height: self.view.bounds.height)
     }
     
     //MARK: - UIView borderViews
@@ -97,6 +146,16 @@ class AddConsultationViewController: UIViewController {
     //MARK: - Actions
     @IBAction func actionArrowDownButton(_ sender: UIButton) {
         invisibleTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func actionHideKeyboardByTap(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    //MARK: - UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+         self.view.endEditing(true)
+        return true
     }
     /*
     // MARK: - Navigation
