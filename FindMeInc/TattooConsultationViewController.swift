b836fd251005d8c75394b9fe5cd097e13bc55fdc
AlexVecher <alex7vecher@gmail.com>
2017-02-28 15:55:40 +0300

    Added TattoConsultationViewController

---
+++ FindMeInc/TattooConsultationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TattooConsultationViewController.swift
+//  FindMeInc
+//
+//  Created by Alex Vecher on 28.02.17.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import UIKit
+
+class TattooConsultationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
