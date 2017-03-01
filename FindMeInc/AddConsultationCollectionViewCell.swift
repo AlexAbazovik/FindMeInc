8903a4d37ce2bf6ec3d8232cd5f177c55e4c11b6
AlexVecher <alex7vecher@gmail.com>
2017-03-01 14:12:07 +0300

    Added AddConsultationScreen

---
+++ FindMeInc/AddConsultationCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AddConsultationCollectionViewCell.swift
+//  FindMeInc
+//
+//  Created by Alex Vecher on 01.03.17.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import UIKit
+
+class AddConsultationCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var deleteImageButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+}
