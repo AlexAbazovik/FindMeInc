096462b1b000c851d9590282b497912857290a5c
AlexVecher <alex7vecher@gmail.com>
2017-03-01 19:48:18 +0300

    Optimized some code.

--- FindMeInc/ArtistPageUserViewController.swift
+++ FindMeInc/ArtistPageUserViewController.swift
@@ -3,20 +3,29 @@ import UIKit
 class ArtistPageUserViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var topLayerView: UIView!
+    
     @IBOutlet weak var bottomLayerViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var topLayerViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var hideAdditionalInfoButtonHeightConstraint: NSLayoutConstraint!
+    
     @IBOutlet weak var moreInfoButton: UIButton!
     @IBOutlet weak var hideAdditionalInfoButton: UIButton!
-    
-    @IBOutlet weak var profilePictureHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var SQUADButtonHeightConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var staffCollectionView: UICollectionView!
     
     @IBOutlet var filteringButtonsCollection: [UIButton]!
  
+    //MARK: - UIViewController lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        staffCollectionView.delegate = self
+        staffCollectionView.dataSource = self
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.isHidden = true
@@ -26,24 +35,11 @@ class ArtistPageUserViewController: UIViewController, UICollectionViewDelegate, 
         self.navigationController?.navigationBar.isHidden = false
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //Specific constraints for iPhone 5 and SE
-        if(UIScreen.main.bounds.size.height == 568){
-            profilePictureHeightConstraint.constant *= 0.8
-            SQUADButtonHeightConstraint.constant *= 0.8
-        }
-        //Specific constraints for iPhone PLUS
-        if(UIScreen.main.bounds.size.height == 736){
-            
-        }
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        staffCollectionView.delegate = self
-        staffCollectionView.dataSource = self
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
+    //MARK: - Actions
     @IBAction func filteringButtonsTap(_ sender: UIButton) {
         for i in filteringButtonsCollection{
             i.isSelected = false
@@ -56,7 +52,7 @@ class ArtistPageUserViewController: UIViewController, UICollectionViewDelegate, 
         let transition = CATransition()
         transition.type = kCATransitionPush
         
-        UIView.animate(withDuration: 0.6, delay: 1.4, options: UIViewAnimationOptions.showHideTransitionViews, animations: {
+        UIView.animate(withDuration: 0.3, delay: 1.4, options: UIViewAnimationOptions.showHideTransitionViews, animations: {
                 transition.subtype = kCATransitionFromBottom
                 self.topLayerView.layer.add(transition, forKey: kCATransition)
                 self.topLayerViewTopConstraint.constant += self.bottomLayerViewHeightConstraint.constant
@@ -77,10 +73,7 @@ class ArtistPageUserViewController: UIViewController, UICollectionViewDelegate, 
         hideAdditionalInfoButton.isEnabled = false
     }
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
+    //MARK: - UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView,
                                  numberOfItemsInSection section: Int) -> Int {
         return 14
@@ -95,20 +88,16 @@ class ArtistPageUserViewController: UIViewController, UICollectionViewDelegate, 
             if indexPath.row % 2 == 0 {
                 cell.backgroundColor = UIColor.white
             }
-        }else if collectionView == self.collectionView{
-            if indexPath.row == 0 && indexPath.section == 0{
+        } else if collectionView == self.collectionView {
+            if indexPath.row == 0 && indexPath.section == 0 {
                 cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellWithButton",
                                                               for: indexPath)
-            }else{
+            } else {
                 cell = collectionView.dequeueReusableCell(withReuseIdentifier: "usualCell",
                                                               for: indexPath)
                 cell.backgroundColor = UIColor.gray
             }
         }
         return cell
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
 }
